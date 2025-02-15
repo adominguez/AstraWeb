23eaf12e6681c7c0bb5bf37b4946eca0003717a5
Adominguez <dmartin.alberto@gmail.com>
2025-02-15 10:23:38 +0100

    chore: Update Astro configuration to include @astrojs/sitemap for generating sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,23 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     contentLayer: true
   },
-  site: 'https://astraweb.dev',
-  integrations: [tailwind(), preact()],
+  site: 'https://astrahub.dev',
+  integrations: [tailwind(), sitemap({
+    serialize(item) {
+      const lastCharacter = item.url.slice(-1);
+      if (lastCharacter === "/") {
+        item.url = item.url.slice(0, -1);
+      }
+      return item;
+    }
+  }), preact()],
   output: 'server',
   adapter: vercel({
     edgeMiddleware: true,
